d0d645dd18e1dde8b0924defe59a0d2bd207d1e6
StefanoM98 <Utente@PC-STEFANO>
2023-09-29 13:01:49 +0200

    Dettagli della pizza mostrati in pagina

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -10,12 +10,25 @@ namespace la_mia_pizzeria_static.Controllers
         {
             using(PizzaContext db = new PizzaContext())
             {
-                List<Pizza> pizze = new List<Pizza>();
+                List<Pizza> pizze = db.pizze.ToList<Pizza>();
 
                 return View("Index", pizze);
              }
         }
 
-        
+        public IActionResult Details(int id)
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                Pizza? pizzaTrovata = db.pizze.Where(pizza=>pizza.Id==id).FirstOrDefault();
+                if(pizzaTrovata == null) 
+                {
+                    return NotFound($"La pizza con {id} non è nel nostro menù");
+                } else
+                {
+                    return View("DettagliPizza", pizzaTrovata);
+                }
+            }
+        }
     }
 }
