09fbebf5ce26aad6935771df9e76cb4fd3d9e174
StefanoM98 <Utente@PC-STEFANO>
2023-09-29 12:37:17 +0200

    Creato il db e il controller della pizza

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,21 @@
+﻿using la_mia_pizzeria_static.Database;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = new List<Pizza>();
+
+                return View("Index", pizze);
+             }
+        }
+
+        
+    }
+}
---
+++ la-mia-pizzeria-static/Database/PizzaContext.cs
@@ -0,0 +1,15 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Database
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> pizze {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=pizza_db;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
