80fd21343dcb8ee56b409980e3a07e6072872fc9
StefanoM98 <Utente@PC-STEFANO>
2023-10-02 12:16:13 +0200

    Inserito il form per la creazione della pizza e le varie validazioni

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -16,6 +16,16 @@ namespace la_mia_pizzeria_static.Controllers
              }
         }
 
+        public IActionResult Userindex()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = db.pizze.ToList<Pizza>();
+
+                return View("Userindex", pizze);
+            }
+        }
+
         public IActionResult Details(int id)
         {
             using(PizzaContext db = new PizzaContext())
@@ -28,6 +38,30 @@ namespace la_mia_pizzeria_static.Controllers
                 {
                     return View("DettagliPizza", pizzaTrovata);
                 }
+            }
+        }
+
+        [HttpGet]
+        public IActionResult CreatePizza()
+        {
+            return View("CreatePizza");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreatePizza(Pizza nuovaPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CreatePizza", nuovaPizza);
+            }
+
+            using(PizzaContext db = new PizzaContext())
+            {
+                db.pizze.Add(nuovaPizza);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
             }
         }
     }
--- la-mia-pizzeria-static/Models/Pizza.cs
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -1,13 +1,25 @@
-﻿namespace la_mia_pizzeria_static.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
 {
     public class Pizza
     {
         
         public int Id {  get; set; }
+
+        [Required(ErrorMessage = "Il nome della pizza è obbligatorio")]
+        [MaxLength(50, ErrorMessage ="Il nome è troppo lungo, nessuno leggerà un nuome del genere")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "La descrizione è obbligatoria")]
+        [MaxLength(200, ErrorMessage = "La descrizione è troppo lunga, nessuno leggerà una descrizione cosi lunga")]
         public string Description { get; set; }
         public string Pathimg {  get; set; }
+
+        [Range(0.01, 20.99)]
         public float Price { get; set; }
+
+        public Pizza() { }
 
         public Pizza(int id, string name, string description, string pathimg, float price) 
         {
